2dd5bff00c6236c4adbef36b0d9e7183533bc5ae
natalliarad <natallia.radaman@gmail.com>
2019-02-10 19:28:30 +0300

    01.03 Edit layout, MainActivity

--- app/src/main/java/com/example/natallia_radaman/sayhello/MainActivity.kt
+++ app/src/main/java/com/example/natallia_radaman/sayhello/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.natallia_radaman.sayhello
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.iid.FirebaseInstanceId
@@ -13,33 +15,78 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private final val TAG = "MainActivity"
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        FirebaseMessaging.getInstance().isAutoInitEnabled = true
 
-        log_token_button.setOnClickListener(object: View.OnClickListener {
-            override fun onClick(v: View?) {
-                // Get token
-                // [START retrieve_current_token]
-                FirebaseInstanceId.getInstance().instanceId.addOnCompleteListener(OnCompleteListener { task ->
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            val channelId = getString(R.string.default_notification_channel_id)
+            val channelName = getString(R.string.default_notification_channel_name)
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager?.createNotificationChannel(
+                NotificationChannel(
+                    channelId,
+                    channelName,
+                    NotificationManager.IMPORTANCE_LOW
+                )
+            )
+        }
+
+        // If a notification message is tapped, any data accompanying the notification
+        // message is available in the intent extras. HERE: when the notification is tapped,
+        // the launcher intent is fired => any accompanying data would be handled here.
+        // If different intent is need to fire => set the click_action field of the
+        // notification message to this intent. The launcher intent is used when no
+        // click_action is specified.
+        //
+        // Handle possible data accompanying notification message.
+        // [START handle_data_extras]
+        intent?.extras?.let{
+            for (key in it.keySet()) {
+                val value = intent.extras.get(key)
+                Log.d(TAG, "Key: $key Value: $value")
+            }
+        }
+        // [END handle_data_extras]
+
+        subscribe_button.setOnClickListener{
+            Log.d(TAG, "Subscribing to weather topic")
+            // [START subscribe_topics]
+            FirebaseMessaging.getInstance().subscribeToTopic("weather")
+                .addOnCompleteListener{task ->
+                    var message = getString(R.string.message_subscribed)
                     if (!task.isSuccessful) {
-                        Log.w(TAG, "getInstanceId failed", task.exception)
-                        return@OnCompleteListener
+                        message = getString(R.string.message_subscribe_failed)
                     }
+                    Log.d(TAG, message)
+                    Toast.makeText(baseContext, message, Toast.LENGTH_SHORT).show()
+                }
+            // [END subscribe_topics]
+        }
+
+        log_token_button.setOnClickListener {
+            // Get token
+            // [START retrieve_current_token]
+            FirebaseInstanceId.getInstance().instanceId
+                .addOnCompleteListener(OnCompleteListener { task ->
+                     if (!task.isSuccessful) {
+                         Log.w(TAG, "getInstanceId failed", task.exception)
+                         return@OnCompleteListener
+                     }
 
                     // Get new Instance ID token
                     val token = task.result?.token
 
                     // Log and toast
-                    val msg = getString(R.string.message_token_fmt, token)
-                    Log.d(TAG, msg)
-                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    val message = getString(R.string.message_token_fmt, token)
+                    Log.d(TAG, message)
+                    Toast.makeText(baseContext, message, Toast.LENGTH_SHORT).show()
                 })
-            }
-        })
+            // [END retrieve_current_token]
+        }
     }
 }
 
